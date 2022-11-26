96cf8763b2e1d1c783101fd332008179fd698178
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-11-26 16:18:35 +0100

    update the project

--- deploy.py
+++ deploy.py
@@ -1,6 +1,10 @@
 from solcx import compile_standard
 import json
 from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -31,31 +35,64 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 # get abi
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
-# for connecting to ganache
-w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-chain_id = 5777
-my_address = "0x4d87b81cc321a3CE0e45aaa88e25f8D543C36973"
+# NEVER hard-code your private key on github
+# for connecting to goerli
+w3 = Web3(
+    Web3.HTTPProvider("https://goerli.infura.io/v3/cb6f2cbe0c894d83941e9c3d15c7993a")
+)
+chain_id = 5
+my_address = "0xA4CBD541B429E2A7a053cA54F8bc969401cb5243"
 # private keys MUST start with 0x, if don't have it, put it
-private_key = "0xf248e2cb9373a887191b721bbc60d59478ef2ca8c4ed39d416ffe4009812a014"
+private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-#Get the latesttest transaction
+# Get the latesttest transaction
 nonce = w3.eth.getTransactionCount(my_address)
-print(nonce)
-#1. Build a transaction
-#2. Sign a transaction
-#3. Send a transaction
-                                            #transaction parameters
-transaction = SimpleStorage.constructor().buildTransaction({
-    "gasPrice": w3.eth.gas_price,
-    "chainID": chain_id, 
-    "from": my_address, 
-    "nonce": nonce
-    })
 
-print(transaction)
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+# transaction parameters
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        # "chainID": chain_id, #TypeError: Transaction must not include unrecognized fields: {'chainID'}
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
 
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+# Send this signed transaction
+print("deploying contract...")
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed!")
 
+# Working with the contract, you always need
+# Contract Adress
+# Contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# Call -> Simulate making the call and getting a return value
+# Transact -> Actually make a state change
 
+# Initial value of favorite number
+print(simple_storage.functions.retrieve().call())
+print("Updating Contract...")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
 
+print(simple_storage.functions.retrieve().call())
